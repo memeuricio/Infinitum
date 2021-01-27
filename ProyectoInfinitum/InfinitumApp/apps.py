f27ba0e852d3922481c0dc004c3175b1c562d36e
memeuricio <78003297+memeuricio@users.noreply.github.com>
2021-01-27 16:17:04 -0300

    Add files via upload

---
+++ ProyectoInfinitum/InfinitumApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InfinitumappConfig(AppConfig):
+    name = 'InfinitumApp'
+
---
+++ ProyectoInfinitum/InfinitumApp/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Contacto
+
+
+
+class ContactoForm(ModelForm):
+    class Meta:
+        model = Contacto
+        fields = '__all__'
